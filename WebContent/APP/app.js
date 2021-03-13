eec57f9e3f5ce5a07461971685d45fcdd71d6446
Filip <filip.zivanac@gmail.com>
2021-03-13 19:44:45 +0100

    Registraciona forma

--- WebContent/APP/app.js
+++ WebContent/APP/app.js
@@ -1,11 +1,13 @@
 const Home  = { template: '<Home></Home>'}
 const Login = { template: '<Login></Login>'}
+const Register = { template: '<Register></Register>'}
 
 const router = new VueRouter({
 	mode : 'hash',
 	routes : [
 		{path: '/', component: Home },
-		{path: '/login', component: Login}
+		{path: '/login', component: Login},
+		{path: '/register', component: Register}
 		
 	]
 	
---
+++ WebContent/APP/register.js
@@ -0,0 +1,35 @@
+Vue.component("Register",{
+	data: function () {
+	    return {    
+	    }
+	},
+	    
+	    template : ` 
+    
+<div id="register-div">
+<link rel="stylesheet" href="CSS/register.css" type="text/css">
+	    	<h1 id="h1-register">Registracija</h1>
+		<form>
+		<table id="reg-table">
+	    	<tr><td>Ime:</td><td><input type="text" name="ime"></td></tr>
+	    	<tr><td>Prezime:</td><td><input type="text" name="prezime"></td></tr>
+	    	<tr><td>Korisničko ime:</td><td><input type="text" name="username"></td></tr>
+	    	<tr><td>Lozinka:</td><td><input type="password" name="password"></td></tr>
+	    	<tr><td>Pol:</td><td><select><option value="M">Muški</option><option value="Z">Ženski</option></select></tr>
+	    	<tr><td>Datum rođenja:</td><td><input type="date" name="datumr"></td></tr>
+	    	<tr><td colspan="2" style="text-align: center; font-size: 30px;"><input type="submit" value="Registruj se"></td></tr>
+		</table>
+		</form>
+		
+</div>
+	  
+`
+	,
+	methods : {
+		init : function() {
+			
+		}
+	}
+		
+
+});
