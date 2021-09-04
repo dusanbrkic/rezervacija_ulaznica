58aac3c73b6f33aa7a97f908e0d261b585d305e4
Filip <filip.zivanac@gmail.com>
2021-09-04 23:46:43 +0200

    provera karata i prodaja karata - back

--- src/dao/ManifestacijeDAO.java
+++ src/dao/ManifestacijeDAO.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import model.Komentar;
 import model.Kupac;
 import model.Manifestacija;
 
@@ -17,18 +18,28 @@ public class ManifestacijeDAO {
     public static String resourceDir;
     
     private static String manifestacijeFileName;
+    private static String komentariFileName;
     
     public static HashMap<String, Manifestacija> manifestacije;
+    public static HashMap<String, Komentar> komentari;
     
 	public ManifestacijeDAO(String realPath) {
 		resourceDir = realPath;
         manifestacijeFileName = resourceDir + fileSeparator + "RES" + fileSeparator + "manifestacije.json";
+        komentariFileName = resourceDir + fileSeparator + "RES" + fileSeparator + "komentari.json";
         loadManifestacije();
 	}
 	
 	public void saveManifestacije() {
 		try {
             new ObjectMapper().writeValue((new FileWriter(manifestacijeFileName)), manifestacije);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	public void saveKomentari() {
+		try {
+            new ObjectMapper().writeValue((new FileWriter(komentariFileName)), komentari);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -43,8 +54,27 @@ public class ManifestacijeDAO {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 	}
+		
+	private void loadKomentari() {
+		TypeReference<HashMap<String, Komentar>> komentariRef
+        = new TypeReference<HashMap<String, Komentar>>() {
+		};
+		try {
+            komentari = new ObjectMapper().readValue(new FileReader(komentariFileName), komentariRef);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public Manifestacija findManifestacija(String key) {
+		
+		if(manifestacije.containsKey(key)) {
+			return manifestacije.get(key);
+		}
+		return null;
+	}
+
 	public void dodajManifestaciju(Manifestacija mf) {
 		String id = mf.getNaziv()+LocalDateTime.now().toString();
 		mf.setId(id);
@@ -52,6 +82,7 @@ public class ManifestacijeDAO {
 		mf.setId(id);
 		mf.setDeleted(false);
 		manifestacije.put(id, mf);
+		saveManifestacije();
 	}
 
 }
